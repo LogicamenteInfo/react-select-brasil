0712c76c6cee412ded64361aa90e9b31a7e33809
Natanael Simões <natanael.simoes@logicamente.info>
2021-12-01 17:25:28 -0400

    Mensagem quando nao houver opcoes

--- src/Components/CommonSelect.tsx
+++ src/Components/CommonSelect.tsx
@@ -16,8 +16,16 @@ type InternalSelectProps = Omit<
 export type SelectProps = Omit<InternalSelectProps, "options">;
 
 export const CommonSelect = (props: InternalSelectProps) => {
-  const value = props.value !== undefined && props.value !== null
-    ? props.options?.find((o) => (o as Option).value === props.value)
-    : null;
-  return <Select noOptionsMessage={() => "Sem opções"} isClearable={true} {...props} value={value as Option} />;
+  const value =
+    props.value !== undefined && props.value !== null
+      ? props.options?.find((o) => (o as Option).value === props.value)
+      : null;
+  return (
+    <Select
+      noOptionsMessage={() => "Sem opções"}
+      isClearable={true}
+      {...props}
+      value={value as Option}
+    />
+  );
 };
